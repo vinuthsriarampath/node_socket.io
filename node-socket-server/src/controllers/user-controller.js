0afd8f759e9429dd246079b58d8989874ce12f27
vinuth <vinuthsriarampath@outlook.com>
2025-10-17 08:00:44 +0530

    feat (node-server): add endpoint to retrieve all online users; integrate with chat socket service

--- node-socket-server/src/controllers/user-controller.js
+++ node-socket-server/src/controllers/user-controller.js
@@ -1,4 +1,5 @@
 import * as userService from '../services/user-service.js';
+import {getOnlineUsers} from "../sockets/chat-socket.js";
 
 export const getCurrentUser = async (req, res) => {
     try {
@@ -13,6 +14,15 @@ export const allUsersExceptMe = async (req,res,next)=> {
     try{
         const users = await userService.getAllUsersExceptMe(req.userId);
         res.status(200).json(users);
+    }catch (err){
+        next(err);
+    }
+}
+
+export const getAllOnlineUsers = async (req,res,next) => {
+    try{
+        const onlineUsers = getOnlineUsers(req.userId);
+        res.status(200).json(onlineUsers);
     }catch (err){
         next(err);
     }
--- node-socket-server/src/routes/user-route.js
+++ node-socket-server/src/routes/user-route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/auth-middleware.js';
-import {allUsersExceptMe, getCurrentUser} from '../controllers/user-controller.js';
+import {allUsersExceptMe, getAllOnlineUsers, getCurrentUser} from '../controllers/user-controller.js';
 
 const router =  express.Router();
 
 router.get('/me',authMiddleware,getCurrentUser);
 router.get('/all-except-me',authMiddleware,allUsersExceptMe);
+router.get('/online',authMiddleware,getAllOnlineUsers)
 
 export default router;
